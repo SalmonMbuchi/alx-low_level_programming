ee909da7ac95d1bd15c7099e87b5520808837d1f
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-10 11:14:53 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print base10 numbers
+ *
+ * Description: prints all base10 numbers and a newline
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	
+	putchar ('\n');
+	return (0);
+}
