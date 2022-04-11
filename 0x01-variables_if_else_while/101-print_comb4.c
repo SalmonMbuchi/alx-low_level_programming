53c56bcd9bcdf1842ef379557fdeb56dafa88830
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-11 01:14:13 -0700

    Possible three digit combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
  *
  * Description: prints a combination of three digits
  *
- * return: Always 0
+ * Return: Always 0(success)
  */
 int main(void)
 {
@@ -15,7 +15,7 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
-			for (num3 = num2 + 1; num3 < 10;num3++)
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
