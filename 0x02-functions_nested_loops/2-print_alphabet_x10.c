4b3a01698669fd949d719ac748999080f319ffa4
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-12 12:29:06 -0700

    Prints the alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,19 @@
  *
  * Description: prints the alphabet ten times in lower case
  *
- * Return: 0 
- */
+ * Return: 0
+ * */
 void print_alphabet_x10(void)
 {
 	int rep = 0;
 	char c;
 	
-	while ( rep++ <= 9)
+	while (rep++ <= 9)
 	{
-		for (c = 'a'; c <= 'z'; c ++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar (c);
-		_putchar('\n');
 		}
 	}
+	_putchar('\n')
 }
