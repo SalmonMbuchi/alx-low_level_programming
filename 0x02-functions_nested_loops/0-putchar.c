65d5ef576e345b4f1c4d3fa59692ec7462a6c986
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-12 09:03:10 -0700

    Prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char x[9]= "_putchar";
+	char x[9] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 8 ; i++)
@@ -18,6 +18,5 @@ int main(void)
 		_putchar(x[i]);
 	}
 	_putchar ('\n');
-	
 	return (0);
 }
