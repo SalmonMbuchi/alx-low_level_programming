2477958c4dfc52b5902d7a59f9a8ca37167860d7
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-12 10:10:13 -0700

    Prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ *
+ * Description: prints alphabet in lowercase
+ *
+ * Return: Always  0(success)
+ */
+int main(void)
+{
+	char q;
+	for (q = 'a'; q <= 'z'; q++)
+	{
+		_putchar("%c", q)
+	}
+	_putchar('\n');
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define main_h_
 
 int _putchar(char c);
+void print_alphabet(void);
 #endif
