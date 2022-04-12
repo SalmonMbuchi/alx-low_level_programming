e42f5f558d4ffacf3d2f2778351fd66c897da8d8
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-12 08:41:39 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *main - prints _putcha
+ *
+ * Description: prints _putchar followed by a newline
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char x[9]= "_putchar";
+	int i = 0;
+
+	for (i = 0; i <=8 ; i++)
+	{
+		_putchar(x[i]);
+	}
+	_putchar ('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h_
+#define main_h_
+
+int _putchar(char c);
+
+#endif
