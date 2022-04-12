6197cd2ec74f70875530dd1127659785df57b65d
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-12 11:34:12 -0700

    Prints

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints the alphabet
+ *
+ * Description: prints the alphabet ten times in lower case
+ *
+ * Return: 0 
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c ++)
+	{
+		char d;
+		while (c ==  d)
+			_putchar(d'\n');
+	_putchar(c);
+	return (0);
+}
+
