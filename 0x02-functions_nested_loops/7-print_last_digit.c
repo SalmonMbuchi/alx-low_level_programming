1a6b86d53514382489da65c8449fc0fdb70634f1
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-13 10:14:47 -0700

    Prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints
+ * Description: prints the last letter of the alphabet
+ *
+ * Return: Always 0
+ */
+int print_last_digit(int)
+{
+	int q;
+	int lastNum;
+
+	lastNum = q % 10;
+	_putchar(lastNum);
+
+	return (0);
+}
