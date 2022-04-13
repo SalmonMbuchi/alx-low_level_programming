fec99a822190be2817421fe437302fb41275f0cb
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-13 03:51:55 -0700

    Checks if c  is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ *
+ * Description: checks if c is lowercase or not
+ *
+ * Return 1(success) or (0) fail
+ */
+int main(void)
+{
+	char c = 'c';
+
+	if (c == 'c')
+		_putchar(1);
+	else if (c == 'C');
+		_putchar(0);
+	return (0);
+}
