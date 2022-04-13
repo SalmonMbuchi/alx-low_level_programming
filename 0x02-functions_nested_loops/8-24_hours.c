2fb5fc09cd7643fb5a890b31aba1c42abe9de829
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-13 11:55:39 -0700

    Prints

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute
+ *
+ * Description: prints every minute of the day 
+ *
+ * Return: 0(success)
+ */
+void jack_bauer(void)
+{
+	int r;
+
+	for (r = 0; r <= 1439; r++)
+	{
+		int l;
+		while (l > r++)
+			_putchar(00:00);
+	_putchar('\n');
+	return (0);
+}
+
