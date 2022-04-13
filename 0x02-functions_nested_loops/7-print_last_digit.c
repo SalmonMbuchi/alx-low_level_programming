625c5fb9d98299b0a2851f42a5d4eb0c87979df4
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-13 10:32:58 -0700

    Prints the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,17 @@
 /**
  * print_last_digit - prints
  * Description: prints the last letter of the alphabet
- *
+ *@q: prints the last digit
  * Return: Always 0
  */
 int print_last_digit(int q)
 {
 	int lastNum;
+	
+	if (q < 0)
+		lastNum = lastNum * -1;
 
-	lastNum = q % 10;
+	lastNum = q % 10;i
 	_putchar(lastNum + '0');
 
 	return (lastNum);
