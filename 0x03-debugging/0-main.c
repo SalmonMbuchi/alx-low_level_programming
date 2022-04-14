412cdf119421fb706d4d336c193c972af99499d9
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-14 02:33:51 -0700

    Positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,8 @@
 
 /**
  *main - determine if number is positive or negative
- *Return:0(Success)
+ *
+ * Return: 0
  */
 int main(void)
 {
@@ -10,6 +11,5 @@ int main(void)
 	
 	i = 0;
 	positive_or_negative(i);
-	
 	return (0);
 }
