0d36ae21d97268d977c6428c7144f3cd6eeca1bd
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-14 00:41:00 -0700

    Pos

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *main - determine if number is positive or negative
+ *
+ *Description: checks whether a number is poditive, negative or zero
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+                printf("%d is positive\n", n);
+        if (n == 0)
+                printf("%d is zero\n", n);
+        if (n < 0)
+                printf("%d is negative\n", n);
+        return (0);
+}
