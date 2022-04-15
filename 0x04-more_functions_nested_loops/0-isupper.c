52ce54012b42434afe7a20dc3472e44fa82c0377
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-15 09:39:12 -0700

    Checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+		_putchar(0);
+	if (c >= 'A' && c <= 'Z')
+		_putchar(1);
+
+	return (0);
+}
