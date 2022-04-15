c7c2ea075defe2f7885d7447f8ccb83c227cbba3
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-15 12:31:31 -0700

    Prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,11 @@
  * print_numbers - prints numbers from 0 to 9
  * Return: 0
  */
-int main(void)
+void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i);
+		_putchar(i + '0');
 	_putchar('\n');
-	return (0);
 }
