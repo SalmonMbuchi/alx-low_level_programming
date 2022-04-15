c169b052d83c5f17c2984a9184965613edbbe90b
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-15 10:14:44 -0700

    LS

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,9 +11,12 @@ int main(void)
 	char c;
 
 	if (c >= 'a' && c <= 'z')
-		_putchar(0);
+	{	
+		putchar(0);
+	}	
 	if (c >= 'A' && c <= 'Z')
-		_putchar(1);
-
+	{	
+		putchar(1);
+	}
 	return (0);
 }
