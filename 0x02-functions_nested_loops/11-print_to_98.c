c10a1cd5f898843da49df7ce839676846a12af0b
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-17 09:59:54 -0700

    Prints upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,25 @@
 void print_to_98(int n)
 {
 	if (n < 98)
+	{
 		printf("%d", n);
 		printf(", ");
 		/*printf(" ");*/
 		n++;
 		if (n == 98)
+		{
 			printf("%d", n);
+		}
+	}
 	else if (n > 98)
+	{
 		printf("%d", n);
-		printf(", ");
+		/*printf(", ");*/
 		n--;
 		if (n == 98)
+		{
 			printf("%d", n);
+		}
+	}
 }
 
