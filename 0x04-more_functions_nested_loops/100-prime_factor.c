a119eab8a20a15108532a0c9abd58227b82527ac
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-18 06:57:53 -0700

    Prints the largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor
+ * Return: always 0
+ */
+int main(void)
+{
+	long int prime = 612852475143;
+	long int quotient = prime;
+	long int divisor = 2;
+
+	while (quotient != divisor)
+	{
+		if (quotient % divisor == 0)
+		{
+			quotient = quotient / divisor;
+		}
+		else
+		{
+			divisor++;
+		}
+	}
+	printf("%li\n", quotient);
+	return (0);
+}
