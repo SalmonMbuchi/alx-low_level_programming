432d81894a3a6397ab8698407d565d3861389487
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-18 11:57:41 -0700

    Prints numbers ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,30 @@
  */
 void more_numbers(void)
 {
-	int c;
+	int line, n, repeat, limit;
 
-	for (c = 0; c <= 10; c++)
+	n = '0';
+	repeat = 0;
+	limit = '9';
+
+	for (line = 0; line < 10; line++)
 	{
-		for (c = 0; c <= 14; c++)
+		while (repeat < 2)
 		{
-			_putchar(c);
+			while (n <= limit)
+			{
+				if (limit == '4')
+					_putchar('1');
+				_putchar(n);
+				n++;
+			}
+			repeat++;
+			limit = '4';
+			n = '0';
 		}
-	_putchar(c);
+		_putchar('\n');
+		repeat = 0;
+		n = '0';
+		limit = '9';
 	}
-	_putchar('\n');
 }
