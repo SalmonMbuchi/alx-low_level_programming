08f694a115830f92f841ab4ea036128a7f92eec1
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-20 23:49:39 -0700

    Updates the value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value to 98
+ * @n: is an integer pointer variable
+ * return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	_putchar(*n + '0');
+}
