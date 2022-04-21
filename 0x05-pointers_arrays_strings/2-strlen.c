0c1eb95dcf91c57820d35c6f817acf49d175a846
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-21 10:36:59 -0700

    Returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: is a character
- * Return: len 
+ * Return: len
  */
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (s[len]!= '\0')
+	while (s[len] != '\0')
 	{
 		len++;
 	}
