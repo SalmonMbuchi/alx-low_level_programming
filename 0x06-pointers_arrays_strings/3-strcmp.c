67bc00b34b10a58e25f9b15b2320f589dbc1a821
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-24 06:35:03 -0700

    Compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,23 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	const unsigned char *s1 = (const unsigned char *)s1;
-	const unsigned char *s2 = (const unsigned char *)s2;
-
-	while (*s1 != '\0')
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		if (*s2 == '\0')
-			return (1);
-		if (*s2 > *s1)
-			return (-1);
-		if (*s1 > *s2)
-			return (1);
-
 		s1++;
 		s2++;
 	}
-	if (*s2 != '\0')
-		return (-1);
-
-	return (0);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
