949485dd5e8424b5d76a7f049da9b2aa3f46f18f
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-27 04:53:13 -0700

    Fills the memory area with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,19 +8,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		if (i % 10)
-		{
-			_putchar(b);
-		}
-		if (!(i % 10) && i)
-		{
-			_putchar('\n');
-		}
-		_putchar(s[i]);
+		s[i] = b;
 	}
 	return (s);
 }
