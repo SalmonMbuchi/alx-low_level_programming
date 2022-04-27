70b13d392d849a2bb32f0e33016af541199f961e
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-27 05:33:31 -0700

    Fills the memory area with a constant byte

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		src[i] > dest[i];
+		src[i] = dest[i];
 	}
 
 	return (dest);
