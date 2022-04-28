29ecb52865c63b7fc58f9fb8c6b6021f3a6f4039
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-28 14:54:23 -0700

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s != '\0')
+	{
+		_putchar(*(s + 0));
+		_puts_recursion((s + 1));
+	}
+	else
+	{
+		_putchar('\n');
 		return;
-	_putchar(*(s + 0);
-	_puts_recursion(s + 1);
-	/*_putchar ('\n');*/
+	}
 }
