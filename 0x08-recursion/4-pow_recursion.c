a2c15724e662d30baeb3eaa00a234194c4169f9d
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-04-28 14:23:39 -0700

    Finds the exponent value of x to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: is an integer
+ * @y: is an integer
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (_pow_recursion(x * _pow_recursion(x, y - 1)));
+}
