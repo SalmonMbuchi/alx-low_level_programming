5fc3f27d5b5978ec7b56b4c65cbb66c9e70a7f27
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-06 01:38:22 -0700

    Create a static library in C

--- 0x09-static_libraries/_puts.c
+++ 0x09-static_libraries/_puts.c
@@ -1,1 +1,17 @@
-void _puts(char *str);
+#include <unistd.h>
+int _putchar(char c);
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
