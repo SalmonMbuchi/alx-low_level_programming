1b18e64eeccbb697c369ca5dfa1d2cd3e3ab3347
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-06 01:24:19 -0700

    Create a static library

--- 0x09-static_libraries/_islower.c
+++ 0x09-static_libraries/_islower.c
@@ -1,4 +1,3 @@
-
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
--- 0x09-static_libraries/_strchr.c
+++ 0x09-static_libraries/_strchr.c
@@ -1,1 +1,20 @@
-char *_strchr(char *s, char c);
+#define NULL 0	
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	if (c == '\0')
+	{
+		return (s + i);
+	}
+	return (NULL);
+}
