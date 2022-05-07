d854be14c553612a9e7527be41d4868f8be32dd3
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-07 02:26:27 -0700

    Initialize an array with a specific character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *ptr = malloc(sizeof(char) * size);
+	
 	if (size  == 0)
 	{
 		return (NULL);
 	}
-	char *ptr;
-	
-	ptr = (char*) malloc(sizeof(char) * size);
 	*ptr = c;
 	free(ptr);
 
