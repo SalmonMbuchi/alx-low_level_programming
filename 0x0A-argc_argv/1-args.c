f37adc4359a13548174d051e4a151e2f541c662b
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-08 10:39:32 -0700

    Prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints the number of arguments passed
+ * @argc: the number of arguments
+ * @argv: an array of command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+}
