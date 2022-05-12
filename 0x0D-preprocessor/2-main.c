a10510f84250fcc28d442e4bb0dbaf78fa50f0ae
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-12 10:29:14 -0700

    A program that prints the file name it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the program name followed by a new line
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/yy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the program name followed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
