c205df08ff1e00af8952de13d1a921e01c8cc066
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-16 13:47:39 -0700

    Searches for an integer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
 /**
  * array_iterator - calls a function given a parameter on each element of array
  * @array: integer array
@@ -8,7 +7,6 @@
  * @action: function pointer
  * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
