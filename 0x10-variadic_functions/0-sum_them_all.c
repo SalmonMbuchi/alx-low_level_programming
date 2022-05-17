8ee93f5471f87f19657d29cb5ef02123edaa0edf
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-17 13:53:27 -0700

    Computes sum of parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list aps;
 	unsigned int i;
-	int sum = 0;
+	int sum;
 
 	if (n == 0)
 		return (0);
 	
 	va_start(aps, n); /*va_start is called with two arguments*/
+	sum = 0;
 	for (i = 0; i < n; i++)
+	{
 		sum = sum + va_arg(aps, int);
+	}
 	va_end(aps);
 
 	return (sum);
