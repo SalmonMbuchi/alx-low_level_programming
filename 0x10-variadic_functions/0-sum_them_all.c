82ce6611dd0cdabe272c3f7a753614ca43481471
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-17 13:20:17 -0700

    Returns the sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,10 +8,9 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ap;
 	unsigned int i;
 	int sum = 0;
-
-	va_list ap; /*declare a variable of type va_list*/
 
 	va_start(ap, n); /*va_start is called with two arguments*/
 	if (n == 0)
