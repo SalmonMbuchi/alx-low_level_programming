41d6acbf777b1e909dca1d53391d61a7cfa12c0c
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-25 11:21:30 -0700

    Adds a node at the end of a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: head of a node
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+
