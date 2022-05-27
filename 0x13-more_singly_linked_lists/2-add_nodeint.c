b1af523df08ceb4ecb087e481152703bb599ba4c
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-27 08:00:40 -0700

    Adds a node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,16 +8,14 @@
  * Return: address of new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
-{	
+{
 	listint_t *new;
-	*head = NULL;	
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		return NULL;
-	else
-		printf("%d\n", n);
+		return (NULL);
+	new->n = n;
 	new->next = *head;
 	*head = new;
-	return (*head);
+	return (new);
 }
