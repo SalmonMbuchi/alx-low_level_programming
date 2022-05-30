1c53965447f74167883b40c33772f07e56f9e2ed
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-05-30 11:37:42 -0700

    Inserts a node at the nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,20 +16,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (head == NULL)/*obsessively check if head is NULL*/
-		return (NULL);	
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+	ptr = *head;
+	node = *head;
 	if (idx == 0)
 	{
 		new->next = node;
 		*head = new;
 		return (new);
 	}
-	ptr = *head;
-	node = *head;
 	while (node != NULL)
 	{
 		if (i == idx)
