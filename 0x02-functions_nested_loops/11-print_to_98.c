5855de60ffd05f09b5d56550c3c1b852f195a4ef
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-06-23 00:57:47 -0700

    Correction for 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,26 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		while (n <= 98)
+		for ( ; n <= 98; n++)
 		{
-			printf("%d", n);
-			printf(", ");
-			n++;
-		if (n == 98)
-			break;
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			else
+				printf("%d, ", n);
 		}
-	}	
-	if (n > 98)
+	}
+	else if (n > 98)
 	{
-		while (n >= 98)
+		for ( ; n >= 98; n--)
 		{
-			printf("%d", n);
-			printf(", ");
-			n--;
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
 		}
 	}
 }
