b110eb5ebcfbdb3753f7e9ea6eeeff2af102f9c1
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-08 12:20:01 +0000

    Add a new node at the beginning of a list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,7 +7,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new, temp;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -15,6 +15,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
+	if (*head != NULL)
+		*head->prev = new;
 	*head = new;
 	return (new);
 }
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a node at the end of a list
+ * @head: pointer to head node
+ * @n: int
+ * Return: pointer to new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *last, temp;
+
+	last = malloc(sizeof(dlistint_t));
+	if (last == NULL)
+		return (NULL);
+	last->n = n;
+	last->next = NULL;
+	last->prev = *head;
+	if (*head == NULL)	
+		*head = last;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = last;
+	}
+	return (last);
+}
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    return (EXIT_SUCCESS);
+}
