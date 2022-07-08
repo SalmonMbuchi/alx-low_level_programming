b5d83573d9cc893889b2ab8b0ba1e5a5b2d17fb5
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-08 12:31:41 +0000

    Add a new node at the end of a list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	last->n = n;
 	last->next = NULL;
 	last->prev = *head;
-	if (*head == NULL)	
+	if (*head == NULL)
 		*head = last;
 	else
 	{
@@ -27,20 +27,4 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		temp->next = last;
 	}
 	return (last);
-}
-int main(void)
-{
-    dlistint_t *head;
-
-    head = NULL;
-    add_dnodeint_end(&head, 0);
-    add_dnodeint_end(&head, 1);
-    add_dnodeint_end(&head, 2);
-    add_dnodeint_end(&head, 3);
-    add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
-    print_dlistint(head);
-    return (EXIT_SUCCESS);
 }
