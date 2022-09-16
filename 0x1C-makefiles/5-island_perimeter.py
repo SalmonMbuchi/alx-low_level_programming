9b3b8a4a196b5a438b0e22e32d106a335a262693
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-09-16 13:31:18 +0300

    Island perimeter correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,14 +7,14 @@ def island_perimeter(grid):
     """grid is a 2D array where 0 represents a water zone"""
     """and 1 represents a land zone"""
     perimeter = 0
-    if grid == None or len(grid) == 0:
+    if (grid == None or len(grid)) == 0:
         return
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
-                if (i > 0 && grid[i - 1][j]) == 1:
+                if (i > 0 and grid[i - 1][j]) == 1:
                     perimeter -= 2
-                if (j > 0 && grid[i][j - 1] == 1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     perimeter -= 2
     return perimeter
