83b22ce70f6d087f4dc6099c143131f11642c43b
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-09-16 13:24:50 +0300

    Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""island perimeter problem"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    """grid is a 2D array where 0 represents a water zone"""
+    """and 1 represents a land zone"""
+    perimeter = 0
+    if grid == None or len(grid) == 0:
+        return
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
