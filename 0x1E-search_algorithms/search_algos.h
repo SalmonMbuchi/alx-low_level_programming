2fdaf9beec07d350e520582e7cd39111746a63c7
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-02 22:57:46 +0300

    advanced tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,12 @@
 #define ALGOS_H
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value
 #endif
