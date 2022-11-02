cb1c0964f956bd56fcaff86652d7a318e65fe840
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-02 21:55:43 +0300

    include newlines

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,16 +9,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
-	int len = size - 1;
+	size_t i;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	for (i = 0; i <= len; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = %d\n", i, array[i]);
+		printf("Value checked array[%ld] = %d\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
