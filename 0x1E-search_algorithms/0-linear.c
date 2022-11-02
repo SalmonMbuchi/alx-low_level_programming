0c2af295bef72175be49efd6a0360a48b2189a63
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-02 21:14:39 +0300

    corrections

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,17 +14,11 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array)
 	{
-		i = 0;
-		while (i <= len)
+		for (i = 0; i <= len; i++)
 		{
+			printf("Value checked array[%d] = %d\n", i, array[i]);
 			if (array[i] == value)
-			{
 				return (i);
-			}
-			else
-				printf("Value checked array[%d] = %d\n", i, array[i]);
-
-			i++;
 		}
 		return (-1);
 	}
